51c9a5a0d994ac3c1e4b18ac9dccf98bb9f2469f
Akash Saini <akash@tech-biblio.localdomain>
2025-12-28 23:01:00 +0530

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+      <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold text-gray-900">
+      Nuxt + Tailwind is working
+    </h1>
+  </div>
+  </div>
+
+</template>
