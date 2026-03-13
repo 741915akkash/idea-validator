24b18644145c4f7243deb0245e1ff26559f047fc
Akash Saini <cheetah74191915@gmail.com>
2026-03-13 15:02:26 +0530

    button style change

--- app/pages/quiz/score.vue
+++ app/pages/quiz/score.vue
@@ -291,7 +291,7 @@
 
           <NuxtLink
             to="/quiz/overview"
-            class="rounded-md bg-white px-4 py-2 text-base font-medium text-black hover:bg-neutral-100"
+            class="rounded-md border border-neutral-400 bg-white px-4 py-2 text-base font-medium text-black hover:bg-neutral-100"
           >
             Back to overview
           </NuxtLink>
